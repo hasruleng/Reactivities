ac81f00b64772e7677f4ab1eebd8b88dfcc3f108
hasrulwho@gmail.com <hasrulwho@gmail.com>
2023-03-25 14:56:44 +0700

    12 Adding Account Controller (Login only), yet wo. Token Service

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+using System.ComponentModel;
+using System.Security.Claims;
+using API.DTOs;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Attributes;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (result)
+            {
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName, 
+                    Image = null,
+                    Token = "This will be a token",
+                    Username = user.UserName
+                };
+            }
+
+            return Unauthorized();
+        }
+
+    }
+}
