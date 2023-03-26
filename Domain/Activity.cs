078947de6084afa6899b2b966d70ada15356a707
hasrulwho@gmail.com <hasrulwho@gmail.com>
2023-03-26 11:49:08 +0700

    14 Configuring the new relationship

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Domain
 {
     public class Activity
@@ -11,5 +9,6 @@ namespace Domain
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        public ICollection<ActivityAttendee> Attendees { get; set; }
     }
 }
