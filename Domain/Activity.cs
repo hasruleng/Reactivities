0435b6aeac4e4dae82d0b897060cf36af04c8cae
hasrulwho@gmail.com <hasrulwho@gmail.com>
2023-03-26 22:34:43 +0700

    14 Adding attendance handler

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -9,6 +9,7 @@ namespace Domain
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        public bool IsCancelled { get; set; } //if the user is the host of an event, then we don't want them to be able to remove themselves from the event, but rather we're going to allow them to cancel the activity itself.
         public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>(); //new list so that we don't get null reference when accessing Attendees
     }
 }
