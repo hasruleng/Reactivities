342831a989ebd9213e114a12b753f626a2476a30
hasrulwho@gmail.com <hasrulwho@gmail.com>
2023-06-01 16:46:09 +0700

    14 ga tahu nih kenapa jadi ada bedanya lokal sama github

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,7 @@
 import { format } from "date-fns";
 import { Link } from "react-router-dom";
-import { Item, Button, Icon, Segment, Label } from "semantic-ui-react";
+import { Item, Button, Icon, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import ActivityListItemAttendee from "./ActivityListItemAttendee";
 
 interface Props {
     activity: Activity
@@ -12,30 +11,12 @@ export default function ActivityListItem({ activity }: Props) {
     return (
         <Segment.Group>
             <Segment>
-                {activity.isCancelled &&
-                    <Label attached='top' color='red' content='Cancelled' style={{ textAlign: 'center' }} />}
                 <Item.Group>
                     <Item>
-                        <Item.Image style={{marginBottom: 5}} size='tiny' circular src='/assets/user.png' />
+                        <Item.Image size='tiny' circular src='/assets/user.png' />
                         <Item.Content>
-                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
-                                {activity.title}
-                            </Item.Header>
-                            <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
-                            {activity.isHost && (
-                                <Item.Description>
-                                    <Label basic color='orange'>
-                                        You are hosting this activity!
-                                    </Label>
-                                </Item.Description>
-                            )}
-                            {activity.isGoing && !activity.isHost && (
-                                <Item.Description>
-                                    <Label basic color='green'>
-                                        You are going to this activity!
-                                    </Label>
-                                </Item.Description>
-                            )}
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Description>Hosted by Bob</Item.Description>
                         </Item.Content>
                     </Item>
                 </Item.Group>
@@ -47,7 +28,7 @@ export default function ActivityListItem({ activity }: Props) {
                 </span>
             </Segment>
             <Segment secondary>
-                <ActivityListItemAttendee attendees={activity.attendees!} />
+                Attendees go here
             </Segment>
             <Segment clearing>
                 <span>{activity.description}</span>
--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -2,9 +2,8 @@ import { format } from 'date-fns';
 import { observer } from 'mobx-react-lite';
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Button, Header, Item, Segment, Image, Label } from 'semantic-ui-react'
-import { Activity } from "../../../app/models/activity";
-import { useStore } from '../../../app/stores/store';
+import {Button, Header, Item, Segment, Image} from 'semantic-ui-react'
+import {Activity} from "../../../app/models/activity";
 
 const activityImageStyle = {
     filter: 'brightness(30%)'
@@ -23,15 +22,11 @@ interface Props {
     activity: Activity
 }
 
-export default observer(function ActivityDetailedHeader({ activity }: Props) {
-    const { activityStore: { updateAttendeance, loading, cancelActivityToggle } } = useStore();
+export default observer (function ActivityDetailedHeader({activity}: Props) {
     return (
         <Segment.Group>
-            <Segment basic attached='top' style={{ padding: '0' }}>
-                {activity.isCancelled &&
-                    <Label style={{ position: 'absolute', zIndex: 1000, left: -14, top: 20 }}
-                        ribbon color='red' content='Cancelled' />}
-                <Image src={`/assets/categoryImages/${activity.category}.jpg`} fluid style={activityImageStyle} />
+            <Segment basic attached='top' style={{padding: '0'}}>
+                <Image src={`/assets/categoryImages/${activity.category}.jpg`} fluid style={activityImageStyle}/>
                 <Segment style={activityImageTextStyle} basic>
                     <Item.Group>
                         <Item>
@@ -39,11 +34,11 @@ export default observer(function ActivityDetailedHeader({ activity }: Props) {
                                 <Header
                                     size='huge'
                                     content={activity.title}
-                                    style={{ color: 'white' }}
+                                    style={{color: 'white'}}
                                 />
                                 <p>{format(activity.date!, 'dd MMM yyyy')}</p>
                                 <p>
-                                    Hosted by <strong><Link to={`/profiles/${activity.hostUsername}`}>{activity.hostUsername}</Link></strong>
+                                    Hosted by <strong>Bob</strong>
                                 </p>
                             </Item.Content>
                         </Item>
@@ -51,34 +46,11 @@ export default observer(function ActivityDetailedHeader({ activity }: Props) {
                 </Segment>
             </Segment>
             <Segment clearing attached='bottom'>
-                {activity.isHost ? (
-                    <>
-                        <Button
-                            color={activity.isCancelled ? 'green' : 'red'}
-                            floated='left'
-                            basic
-                            content={activity.isCancelled ? 'Re-activate activity' : 'Cancel Activity'}
-                            onClick={cancelActivityToggle}
-                            loading={loading}
-                        />
-                        <Button
-                            as={Link}
-                            to={`/manage/${activity.id}`}
-                            color='orange'
-                            floated='right'
-                            disabled={activity.isCancelled}
-                        >
-                            Manage Event
-                        </Button>
-                    </>
-
-                ) : activity.isGoing ? (
-                    <Button onClick={updateAttendeance} 
-                        loading={loading}>Cancel attendance</Button>
-                ) : (
-                    <Button disabled={activity.isCancelled} onClick={updateAttendeance} 
-                        loading={loading} color='teal'>Join Activity</Button>
-                )}
+                <Button color='teal'>Join Activity</Button>
+                <Button>Cancel attendance</Button>
+                <Button as={Link} to={`/manage/${activity.id}`} color='orange' floated='right'>
+                    Manage Event
+                </Button>
             </Segment>
         </Segment.Group>
     )
